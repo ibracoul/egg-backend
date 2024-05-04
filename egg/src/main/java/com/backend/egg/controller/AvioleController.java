994bebfbbfe743193138828468f8797576198b18
Ibrahima Coulibaly <coulibra4@gmail.com>
2024-05-04 18:34:49 +0200

    added the configuration to resolve erreur CORS

--- egg/src/main/java/com/backend/egg/controller/AvioleController.java
+++ egg/src/main/java/com/backend/egg/controller/AvioleController.java
@@ -4,16 +4,14 @@ import com.backend.egg.entity.Alveole;
 import com.backend.egg.entity.AlveoleParams;
 import com.backend.egg.repository.AlveoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:4200")
 public class AvioleController {
     @Autowired
     private final AlveoleRepository alveoleRepository;
@@ -23,12 +21,8 @@ public class AvioleController {
     }
 
     @PostMapping("/api/v1/aviole/add")
+
     public void add(@RequestBody Alveole alveole){
-        /*Alveole alveole = new Alveole();
-        alveole.setNombre(params.nombre());
-        alveole.setPrix(params.prix());
-        alveole.setDateSortie(params.date());
-        alveole.setTaille(params.taille());*/
 
         alveoleRepository.save(alveole);
     }
--- egg/src/main/java/com/backend/egg/entity/Alveole.java
+++ egg/src/main/java/com/backend/egg/entity/Alveole.java
@@ -10,39 +10,47 @@ public class Alveole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private int nombre;
-    private double prix;
-    @Temporal(TemporalType.DATE)
-    private LocalDate dateSortie;
+    private int quantite;
+    private double prix_uni;
+    private double prix_tt;
     @Enumerated(EnumType.STRING)
     private Type taille;
-
+    @Temporal(TemporalType.DATE)
+    private LocalDate wati;
 
     public Alveole() {
     }
 
-
-    public Alveole(int nombre, double prix, LocalDate dateSortie, Type taille) {
-        this.nombre = nombre;
-        this.prix = prix;
+    public Alveole(int quantite, double prix_uni, double prix_tt, Type taille, LocalDate wati) {
+        this.quantite = quantite;
+        this.prix_uni = prix_uni;
+        this.prix_tt = prix_tt;
         this.taille = taille;
-        this.dateSortie = dateSortie;
+        this.wati = wati;
     }
 
-    public int getNombre() {
-        return nombre;
+    public int getQuantite() {
+        return quantite;
     }
 
-    public void setNombre(int nombre) {
-        this.nombre = nombre;
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
     }
 
-    public double getPrix() {
-        return prix;
+    public double getPrix_uni() {
+        return prix_uni;
     }
 
-    public void setPrix(double prix) {
-        this.prix = prix;
+    public void setPrix_uni(double prix_uni) {
+        this.prix_uni = prix_uni;
+    }
+
+    public double getPrix_tt() {
+        return prix_tt;
+    }
+
+    public void setPrix_tt(double prix_tt) {
+        this.prix_tt = prix_tt;
     }
 
     public Type getTaille() {
@@ -53,11 +61,11 @@ public class Alveole {
         this.taille = taille;
     }
 
-    public LocalDate getDateSortie() {
-        return dateSortie;
+    public LocalDate getWati() {
+        return wati;
     }
 
-    public void setDateSortie(LocalDate dateSortie) {
-        this.dateSortie = dateSortie;
+    public void setWati(LocalDate wati) {
+        this.wati = wati;
     }
 }
